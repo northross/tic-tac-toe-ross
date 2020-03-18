d9190511262143bf7ad32ee907a374ff42ee7edb
northross <rossnorthrup@yahoo.com>
2020-03-17 23:22:57 -0400

    API

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -5,12 +5,13 @@ const config = require('../config')
 const store = require('../store')
 const ui = require('./ui.js')
 
-const signUp = function (data) {
-  console.log('In api.js')
+const signUp = function (credentials) {
+  console.log('Sign up', credentials)
+  console.log(config.apiUrl + '/sign-up')
   return $.ajax({
     url: config.apiUrl + '/sign-up',
     method: 'POST',
-    data
+    data: credentials
   })
 }
 
@@ -19,7 +20,7 @@ const signIn = function (data) {
   return $.ajax({
     url: config.apiUrl + '/sign-in',
     method: 'POST',
-    data
+    data: data
   })
 }
 
@@ -46,9 +47,42 @@ const signOut = function () {
   })
 }
 
+const newGame = function (data) {
+  console.log(data)
+  console.log('In api.js')
+  return $.ajax({
+    url: config.apiUrl + '/games',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const updateGame = function (spot, player) {
+  console.log('updateGame')
+  return $.ajax({
+    url: config.apiUrl + '/games/' + store.game.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }, data: {
+    "game": {
+      "cell": {
+        "index": spot,
+        "value": player
+      },
+      "over": false
+    }
+  }
+  })
+}
+
 module.exports = {
   signUp,
   signIn,
   signOut,
-  changePassword
+  changePassword,
+  newGame,
+  updateGame
 }
