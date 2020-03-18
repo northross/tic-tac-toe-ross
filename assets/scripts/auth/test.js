1e98f7e09029c08fbdbde02e5ecba77993bd8bed
northross <rossnorthrup@yahoo.com>
2020-03-17 23:39:24 -0400

    File for testing game logic

--- assets/scripts/auth/test.js
+++ assets/scripts/auth/test.js
@@ -1,36 +1,36 @@
+//
+// const test = function (games.cells) {
+// if (games.cells[0].getElementbyId === games.cells[1].getElementbyId && games.cells[0].getElementbyId === games.cells[2].getElementbyId) {
+//   return winner
+//   } else if (games.cells[0].getElementbyId === games.cells[3].getElementbyId && games.cells[0].getElementbyId === games.cells[6].getElementbyId) {
+//     return winner
+//   } else if(games.cells[0].getElementbyId === games.cells[4].getElementbyId && games.cells[0].getElementbyId === games.cells[3].getElementbyId) {
+//     return winner
+//   } else if(games.cells[1].getElementbyId === games.cells[4].getElementbyId && games.cells[1].getElementbyId === games.cells[7].getElementbyId) {
+//     return winner
+//   } else if(games.cells[2].getElementbyId === games.cells[4].getElementbyId && games.cells[2].getElementbyId === games.cells[6].getElementbyId) {
+//     return winner
+//   } else if(games.cells[2].getElementbyId === games.cells[5].getElementbyId && games.cells[2].getElementbyId === games.cells[8].getElementbyId) {
+//     return winner
+//   } else if(games.cells[3].getElementbyId === games.cells[4].getElementbyId && games.cells[3].getElementbyId === games.cells[5].getElementbyId) {
+//     return winner
+//   } else if(games.cells[6].getElementbyId === games.cells[7].getElementbyId && games.cells[6].getElementbyId === games.cells[8].getElementbyId) {
+//     return winner
+//   }
+//
+//
+// const input = function (clickId, test)
+// for (let i = 0; winner || endgame; i++) {
+//   if (games.cells[id] === "x" || games.cells[id] === "o") {
+//     return
+//   } else if (i % 2 = 0) {
+//     games.cells[].splice([array id], 0, "x")
+//   } else if (i % 2 = 1) {
+//     games.cell[].splice([array id], 0, "o")
+//   }
+// }
+//
 
-const test = function (games.cells) {
-if (games.cells[0].getElementbyId === games.cells[1].getElementbyId && games.cells[0].getElementbyId === games.cells[2].getElementbyId) {
-  return winner
-  } else if (games.cells[0].getElementbyId === games.cells[3].getElementbyId && games.cells[0].getElementbyId === games.cells[6].getElementbyId) {
-    return winner
-  } else if(games.cells[0].getElementbyId === games.cells[4].getElementbyId && games.cells[0].getElementbyId === games.cells[3].getElementbyId) {
-    return winner
-  } else if(games.cells[1].getElementbyId === games.cells[4].getElementbyId && games.cells[1].getElementbyId === games.cells[7].getElementbyId) {
-    return winner
-  } else if(games.cells[2].getElementbyId === games.cells[4].getElementbyId && games.cells[2].getElementbyId === games.cells[6].getElementbyId) {
-    return winner
-  } else if(games.cells[2].getElementbyId === games.cells[5].getElementbyId && games.cells[2].getElementbyId === games.cells[8].getElementbyId) {
-    return winner
-  } else if(games.cells[3].getElementbyId === games.cells[4].getElementbyId && games.cells[3].getElementbyId === games.cells[5].getElementbyId) {
-    return winner
-  } else if(games.cells[6].getElementbyId === games.cells[7].getElementbyId && games.cells[6].getElementbyId === games.cells[8].getElementbyId) {
-    return winner
-  }
-
-
-const input = function (clickId, test)
-for (let i = 0; winner || endgame; i++) {
-  if (games.cells[id] === "x" || games.cells[id] === "o") {
-    return
-  } else if (i % 2 = 0) {
-    games.cells[].splice([array id], 0, "x")
-  } else if (i % 2 = 1) {
-    games.cell[].splice([array id], 0, "o")
-  }
-}
-
-const button = function ()
 //
 // (games.cells[id] === "") {
   // games.cells[id].splice(path, 0, "x")
