55aaec4770b34316b9042be86317a222ccce94c8
northross <rossnorthrup@yahoo.com>
2020-03-17 23:14:57 -0400

    Update Game Board Curl Request

---
+++ curl-scripts/updateGame.sh
@@ -0,0 +1,18 @@
+'use strict'
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+  "game": {
+    "cell": {
+      "index": 0,
+      "value": "x"
+    },
+    "over": false
+  }
+}'
+
+echo
