0ec3ea9b80e24359cb0e61d8cf69a3eb91c929a3
northross <rossnorthrup@yahoo.com>
2020-06-27 14:41:07 -0400

    no cors fix failed

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cors-anywhere.herokuapp.com/https://stormy-beyond-54635.herokuapp.com',
-  development: 'https://cors-anywhere.herokuapp.com/http://localhost:4741'
+  production: 'https://stormy-beyond-54635.herokuapp.com',
+  development: 'http://localhost:4741'
   // https://tic-tac-toe-wdi.herokuapp.com
 }
 
